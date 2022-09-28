474178c7d84a88355c53f93af9afe5cc08faf781
KhanhhNe <50789229+KhanhhNe@users.noreply.github.com>
2022-09-28 22:12:05 +0700

    Update install-docker-compose-centos.sh

--- install-docker-compose-centos.sh
+++ install-docker-compose-centos.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
